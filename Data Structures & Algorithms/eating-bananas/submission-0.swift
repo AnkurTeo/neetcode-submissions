6e19e79ff695560f72bcecc0be5e590982fc3abc
AnkurTeo <100286205+AnkurTeo@users.noreply.github.com>
2026-05-26 09:27:21 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.swift
@@ -0,0 +1,25 @@
+class Solution {
+    func minEatingSpeed(_ piles: [Int], _ h: Int) -> Int {
+        var low = 1
+        var high = piles.max()!
+
+        func canEat(_ capacity: Int) -> Bool {
+            var ch = 0
+            for pile in piles {
+                ch = ch + Int(ceil(Double(pile)/Double(capacity)))
+            }
+
+            return ch <= h
+        }
+
+        while low < high {
+            let mid = ( low + high ) / 2
+            if canEat(mid) {
+                high = mid 
+            } else {
+                low = mid + 1
+            }
+        }
+        return high
+    }
+}
